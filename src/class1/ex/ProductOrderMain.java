6d79ad26f540fee15c9b5cdeeec18460eea8eb20
owuhye <owuhye@naver.com>
2025-04-21 22:32:35 +0900

    20250421

--- src/class1/ex/ProductOrderMain.java
+++ src/class1/ex/ProductOrderMain.java
@@ -7,6 +7,7 @@ public class ProductOrderMain {
         ProductOrder[] orders = new ProductOrder[3];
 
         ProductOrder order1 = new ProductOrder();
+
         order1.productName = "두부";
         order1.price = 2000;
         order1.quantity = 2;
